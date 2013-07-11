e92c78ee7ac6bc8d6dba5a2877d7ed3d81f7f7e2
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-07-11 18:17:26 -0400

    Don't use global variables... dummy

--- lib/ee_cummings/values.rb
+++ lib/ee_cummings/values.rb
@@ -1,12 +1,12 @@
 module EECummings
   module Values
     def warehouse=(warehouse)
-      $warehouse=warehouse
+      @@warehouse=warehouse
     end
 
     def method_missing(meth, *args, &block)
       key = meth.upcase.to_s
-      $warehouse.get_value(key) || super
+      @@warehouse.get_value(key) || super
     end
   end
 end
